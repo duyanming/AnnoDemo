c39ac87391597a6e6b48d9f0c3cb8dd884f4ec02
duyanming <dym880@163.com>
2019-12-06 13:56:48 +0800

    升级类库 调整监控

--- traceWeb/wwwroot/js/scripts/welcome.js
+++ traceWeb/wwwroot/js/scripts/welcome.js
@@ -8,6 +8,7 @@ $(function () {
     //input.endDate = "2019-10-01";
     bif.process(input, function (data) {
         var myChart = echarts.init(document.getElementById('trace'));
+        var myChartPie = echarts.init(document.getElementById('tracePie'));
         // 指定图表的配置项和数据
         var option = {
             color: ['#3398DB'],
@@ -49,8 +50,40 @@ $(function () {
 
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
+        var pieData = [];
+        if (data.outputData.xAxis) {
+            for (var i = 0; i < data.outputData.xAxis.length; i++) {
+                pieData.push({ value: data.outputData.values[i], name: data.outputData.xAxis[i]});
+            }
+        }
+        var optionPie = {
+          
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+                data: data.outputData.xAxis
+            },
+            series: [{
+                name: '访问量',
+                type: 'pie',
+                radius: '80%',
+                center: ['50%', '60%'],
+                data: pieData
+            }]
+        };
+        myChartPie.setOption(optionPie);
+
         defaultService = data.outputData.xAxis[0];
         myChart.on('click', function (params) {
+            if (params.name !== defaultService) {
+                SetWatch(connection, params.name);
+            }
+        });
+        myChartPie.on('click', function (params) {
             if (params.name !== defaultService) {
                 SetWatch(connection, params.name);
             }
@@ -63,6 +96,8 @@ $(function () {
 var defaultService = "WebApi";
 var data = [];
 var memorydata = [];
+var dataSystem = [];
+var memorydataSystem = [];
 var date = [];
 var cpuChart = null;
 var memoryChart = null;
@@ -71,20 +106,29 @@ function CpuInt() {
     cpuChart = echarts.init(document.getElementById('cpu'));
     //CPU动态图
     var option = {
-        title: {
-            left: 'center',
-            text: 'CPU监控'
+
+        legend: {
+            left: 'left',
+            data: ['App', 'System']
         },
         toolbox: {
             feature: {
-                dataZoom: {
-                    yAxisIndex: 'none'
-                },
-                restore: {},
                 saveAsImage: {}
             }
         },
         tooltip: {
+            formatter: function (params) {
+                var msg = "";
+                if (params.length > 0) {
+                    msg += params[0].name + "<br/>";
+                }
+                for (var i = 0; i < params.length; i++) {
+                    msg += params[i].marker + " "
+                        + params[i].seriesName + ": " + params[i].value + "%<br/>";
+                }
+                return msg;
+            },
+            hideDelay: 1500,
             trigger: 'axis'
         },
         xAxis: {
@@ -114,7 +158,7 @@ function CpuInt() {
             }
         }],
         series: [{
-            name: 'CPU使用率',
+            name: 'App',
             type: 'line',
             smooth: true,
             symbol: 'none',
@@ -122,16 +166,31 @@ function CpuInt() {
             itemStyle: {
                 color: 'rgb(255, 70, 131)'
             },
-            areaStyle: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                    offset: 0,
-                    color: 'rgb(255, 158, 68)'
-                }, {
-                    offset: 1,
-                    color: 'rgb(255, 70, 131)'
-                }])
+            areaStyle: {},
+            data: data,
+            markPoint: {
+                data: [
+                    { type: 'max', name: '最大值' },
+                    { type: 'min', name: '最小值' }
+                ]
+            }
+        }, {
+            name: 'System',
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            sampling: 'average',
+            itemStyle: {
+                color: 'rgb(100 ,149, 237)'
             },
-            data: data
+            areaStyle: {},
+            data: dataSystem,
+            markPoint: {
+                data: [
+                    { type: 'max', name: '最大值' },
+                    { type: 'min', name: '最小值' }
+                ]
+            }
         }]
     };
     cpuChart.setOption(option);
@@ -142,20 +201,28 @@ function MemoryInt() {
     memoryChart = echarts.init(document.getElementById('memory'));
     //CPU动态图
     var option = {
-        title: {
-            left: 'center',
-            text: 'Memory监控'
+        legend: {
+            left: 'left',
+            data: ['App', 'System']
         },
         toolbox: {
             feature: {
-                dataZoom: {
-                    yAxisIndex: 'none'
-                },
-                restore: {},
                 saveAsImage: {}
             }
         },
         tooltip: {
+            formatter: function (params) {
+                var msg = "";
+                if (params.length > 0) {
+                    msg += params[0].name + "<br/>";
+                }
+                for (var i = 0; i < params.length; i++) {
+                    msg += params[i].marker + " "
+                        + params[i].seriesName + ": " + params[i].value + "M(" + (params[i].value / 1024).toFixed(2) + "G) <br/>";
+                }
+                return msg;
+            },
+            hideDelay: 1500,
             trigger: 'axis'
         },
         xAxis: {
@@ -171,7 +238,8 @@ function MemoryInt() {
             type: 'inside',
             start: 0,
             end: 100
-        }, {
+        },
+        {
             start: 0,
             end: 10,
             handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
@@ -185,7 +253,7 @@ function MemoryInt() {
             }
         }],
         series: [{
-            name: '内存占用（M）',
+            name: 'App',
             type: 'line',
             smooth: true,
             symbol: 'none',
@@ -193,16 +261,32 @@ function MemoryInt() {
             itemStyle: {
                 color: 'rgb(255, 70, 131)'
             },
-            areaStyle: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                    offset: 0,
-                    color: 'rgb(255, 158, 68)'
-                }, {
-                    offset: 1,
-                    color: 'rgb(255, 70, 131)'
-                }])
+            areaStyle: {},
+            data: memorydata,
+            markPoint: {
+                data: [
+                    { type: 'max', name: '最大值' },
+                    { type: 'min', name: '最小值' }
+                ]
+            }
+        },
+        {
+            name: 'System',
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            sampling: 'average',
+            itemStyle: {
+                color: 'rgb(100 ,149, 237)'
             },
-            data: memorydata
+            areaStyle: {},
+            data: memorydataSystem,
+            markPoint: {
+                data: [
+                    { type: 'max', name: '最大值' },
+                    { type: 'min', name: '最小值' }
+                ]
+            }
         }]
     };
     memoryChart.setOption(option);
@@ -221,28 +305,48 @@ function StartMonitoring() {
     connection.on("SendMonitorData", function (_data) {
         var _date = new Date(_data.currentTime);
         date.push([_date.getHours(), _date.getMinutes(), _date.getSeconds()].join(':'));
+
         data.push(_data.cpu);
+        dataSystem.push((_data.cpuTotalUse).toFixed(2));
+
         memorydata.push((_data.memory).toFixed(2));
-        if (data.length > 1200) {
+        memorydataSystem.push((_data.memoryTotalUse).toFixed(2));
+
+        if (data.length > 600) {
+
             memorydata.shift();
+            memorydataSystem.shift();
+
             data.shift();
+            dataSystem.shift();
+
             date.shift();
         }
         cpuChart.setOption({
             xAxis: {
                 data: date
             },
-            series: [{
-                data: data
-            }]
+            series: [
+                {
+                    data: data
+                },
+                {
+                    data: dataSystem
+                }
+            ]
         });
         memoryChart.setOption({
+            title: { subtext: "系统总内存：" + (_data.memoryTotal / 1024).toFixed(2) + "G" },
             xAxis: {
                 data: date
             },
-            series: [{
-                data: memorydata
-            }]
+            series: [
+                {
+                    data: memorydata
+                },
+                {
+                    data: memorydataSystem
+                }]
         });
     });
     connection.on("OnConnected", function (_data) {
@@ -267,14 +371,16 @@ function SetWatch(connection, name) {
     date = [];
     data = [];
     memorydata = [];
+    dataSystem = [];
+    memorydataSystem = [];
     defaultService = name;
     connection.invoke("SetWatch", name).catch(function (err) {
         console.error(err.toString());
     });
     cpuChart.setOption({
-        title: { left: 'center', text: name + '--CPU监控' }
+        title: { left: 'center', text: name + '-CPU使用率' }
     });
     memoryChart.setOption({
-        title: { left: 'center', text: name + '--Memory监控' }
+        title: { left: 'center', text: name + '-Memory使用率' }
     });
 }
