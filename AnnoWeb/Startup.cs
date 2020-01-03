069d79d75a5d7d6ddaaebe8c5609cb1d05cb80ab
duyanming <dym880@163.com>
2020-01-03 14:00:45 +0800

    静态资源压缩

--- AnnoWeb/Startup.cs
+++ AnnoWeb/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -19,6 +20,11 @@ namespace traceWeb
             services.AddSingleton<Hubs.TaskManager, Hubs.TaskManager>();
             services.AddMvc();
             services.AddSignalR();
+            services.AddResponseCompression(options =>
+            {
+                //options.Providers.Add<BrotliCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "image/svg+xml" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +39,8 @@ namespace traceWeb
                 app.UseExceptionHandler("/error/Index/500");
                 app.UseStatusCodePagesWithReExecute("/error/Index/404");
             }
+            app.UseResponseCompression();
+
             app.UseStaticFiles();//使用静态文件默认的文件夹为wwwroot
             app.UseRouting();
             app.UseEndpoints(endpoints =>
